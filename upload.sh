871c26c74060a72bd3b8de1067f8fa51c100fcea
ray <rayjlim1@gmail.com>
2019-12-07 20:52:02 -0800

    upload script

--- upload.sh
+++ upload.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
-export FTP_HOST=''
-export FTP_USER=''
-export FTP_TARGETFOLDER=''
+. ./upload.sh
+PREP_DIR='../cal_track_prod'
+mkdir $PREP_DIR
+
+rsync -ravz  --exclude-from 'exclude-list.txt' --delete . $PREP_DIR
+rsync -avz  _config/SERVER_CONFIG.php.prod $PREP_DIR/backend/SERVER_CONFIG.php
+rsync -avz  .htaccess $PREP_DIR/
+
+pushd .
+cd $PREP_DIR
+
+/usr/local/bin/composer install  --no-dev
+
+echo "start upload"
+
+# # setup passwordless ssh
+# ssh-keygen -f "/home/ray/.ssh/known_hosts" -R $FTP_HOST
+# ssh-copy-id -f -i ~/.ssh/id_rsa -oHostKeyAlgorithms=+ssh-dss $FTP_USER@$FTP_HOST
+
+rsync -rave  'ssh -oHostKeyAlgorithms=+ssh-dss' --delete . $FTP_USER@$FTP_HOST:$FTP_TARGETFOLDER 
+
+popd
