603f29760ed0b48c737d34c078b4b87a5b88bd88
ray <rayjlim1@gmail.com>
2020-05-21 21:28:00 -0700

    fix: :bug: remove debug

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php namespace tracker;
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+define('_PS_MODE_DEV_', true);
 ini_set('MAX_EXECUTION_TIME', 3600);
 session_start();
 
