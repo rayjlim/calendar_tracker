25e0385d6f3ccac8726165f4823cb82f69ebbfab
ray <rayjlim1@gmail.com>
2020-08-29 14:09:27 -0700

    wip: 🚧 initial commit for react-native-web

---
+++ ui-react-native-web/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  View,
+} from 'react-native';
+import Home from './home';
+import Header from './header';
+
+class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.appContainer}>
+        <Header title="Tracker" />
+        <Home />
+        <p>learn react</p>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  appContainer: {
+    flex: 1,
+  },
+});
+
+AppRegistry.registerComponent('App', () => App);
+
+export default App;
