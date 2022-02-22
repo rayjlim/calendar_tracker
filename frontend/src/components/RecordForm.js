65bb1aae8d621477581066211c7f2f93ed4bd7d8
ray <rayjlim1@gmail.com>
2022-02-21 16:54:50 -0800

    feat: ✨ #32 date picker for input, improve state mgmt

--- frontend/src/components/RecordForm.js
+++ frontend/src/components/RecordForm.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
-import format from 'date-fns/format';
+import React, { useState } from 'react';
+import { format, sub } from 'date-fns';
+import DatePicker from 'react-date-picker';
 import {
   Text,
   TextInput,
@@ -7,48 +8,51 @@ import {
   View,
   TouchableHighlight,
   StyleSheet,
-} from "react-native";
-import Constants from "../constants";
+} from 'react-native';
+import Constants from '../constants';
+
+const FULL_DATE_FORMAT = 'yyyy-MM-dd';
+const DEFAULT_COUNT = 140.0;
 
 const styles = StyleSheet.create({
   actionsContainer: {
     flex: 1,
-    flexDirection: "row",
-    justifyContent: "flex-end",
-    alignItems: "center",
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
     padding: 10,
   },
   actionButton: {
     padding: 10,
-    color: "white",
+    color: 'white',
     borderRadius: 6,
     width: 80,
-    backgroundColor: "#808080",
+    backgroundColor: '#808080',
     marginRight: 5,
     marginLeft: 5,
   },
   defaultButton: {
-    width: "98%",
+    width: '98%',
   },
   actionButtonDestructive: {
-    backgroundColor: "#ff4b21",
+    backgroundColor: '#ff4b21',
   },
   actionButtonText: {
-    textAlign: "center",
+    textAlign: 'center',
   },
   countInput: {
-    width: "7em",
-    backgroundColor: "#FFF",
-    border: "1px solid #EEE",
+    width: '7em',
+    backgroundColor: '#FFF',
+    border: '1px solid #EEE',
   },
-  dateInput: {
-    width: "7em",
-    backgroundColor: "#FFF",
-    border: "1px solid #EEE",
+  commentInput: {
+    width: '25em',
+    backgroundColor: '#FFF',
+    border: '1px solid #EEE',
   },
   centering: {
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
     padding: 8,
     // height: "100vh",
     // flexDirection: "row",
@@ -57,63 +61,60 @@ const styles = StyleSheet.create({
   },
 });
 
-const RecordForm = ({onUpdate}) => {
-  const [state, setState] = useState({
-    recordDate: format(new Date(), 'yyyy-MM-dd'),
-    count: 140.0,
-    comment: ""
-  });
+const RecordForm = ({ onUpdate }) => {
+  const [recordDate, setRecordDate] = useState(new Date());
+  const [count, setCount] = useState(DEFAULT_COUNT);
+  const [comment, setComment] = useState('');
 
-  const onTextChange = (name) => (value) => {
-    const updated = {};
-    updated[name] = value;
-    setState({ ...state, ...updated });
-    // setState( Object.assign(state, { [name]: value }));
+  const calendarCheck = () => {
+    if (recordDate === null) {
+      setRecordDate(new Date());
+    }
   };
 
   async function sendRecord() {
-    console.log("sendRecord");
-    console.log("state", state);
+    console.log('sendRecord');
 
-    if (state.count > 175 || state.count < 115){ //magic numbers
+    if (count > 175 || count < 115) {
+      //magic numbers
       alert('Invalid number');
       return;
     }
     const data = {
-      date: state.recordDate,
-      count: state.count,
-      comment: state.comment,
-      goal: "weight",
+      date: format(recordDate, FULL_DATE_FORMAT, new Date()),
+      count: count,
+      comment: comment,
+      goal: 'weight',
     };
     const url = `${Constants.REST_ENDPOINT}record/`;
     try {
       const response = await fetch(url, {
-        method: "POST", // *GET, POST, PUT, DELETE, etc.
-        mode: "cors", // no-cors, *cors, same-origin
-        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: "same-origin", // include, *same-origin, omit
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
           // 'Content-Type': 'application/x-www-form-urlencoded',
         },
-        redirect: "follow", // manual, *follow, error
-        referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        redirect: 'follow', // manual, *follow, error
+        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
         body: JSON.stringify(data), // body data type must match "Content-Type" header
       });
       console.log(response);
       if (response.ok) {
-        alert("Save Complete");
+        alert('Save Complete');
         await onUpdate();
-        setState({ ...state,
-          recordDate: format(new Date(), 'yyyy-MM-dd'),
-          count: 140.0,
-          comment: ""
-        });
+
+        //reset values
+        setRecordDate(new Date());
+        setCount(DEFAULT_COUNT);
+        setComment('');
       } else {
-        console.log("Network response was not ok.");
+        console.log('Network response was not ok.');
       }
     } catch (error) {
-      alert("Error: " + error);
+      alert('Error: ' + error);
     }
   }
 
@@ -126,20 +127,19 @@ const RecordForm = ({onUpdate}) => {
           styles.defaultButton,
         ]}
         onPress={() => {
-          const updated = { count: 140.0 };
-          setState({ ...state, ...updated });
+          setCount(DEFAULT_COUNT);
         }}
       >
-        <Text style={styles.actionButtonText}>Default: 140.0</Text>
+        <Text style={styles.actionButtonText}>Default: {DEFAULT_COUNT}</Text>
       </TouchableHighlight>
 
       <View style={styles.actionsContainer}>
         <TouchableHighlight
           style={[styles.actionButton]}
           onPress={() => {
-            const factor = -0.2;
-            const updated = { count: +(state.count + factor).toFixed(2) };
-            setState({ ...state, ...updated });
+            const factor = 0.2;
+            const updated = +(count + factor).toFixed(2);
+            setCount(updated);
           }}
         >
           <Text style={styles.actionButtonText}>-.2</Text>
@@ -148,8 +148,8 @@ const RecordForm = ({onUpdate}) => {
           style={[styles.actionButton]}
           onPress={() => {
             const factor = -1.0;
-            const updated = { count: +(state.count + factor).toFixed(2) };
-            setState({ ...state, ...updated });
+            const updated = +(count + factor).toFixed(2);
+            setCount(updated);
           }}
         >
           <Text style={styles.actionButtonText}>-1</Text>
@@ -158,8 +158,8 @@ const RecordForm = ({onUpdate}) => {
           style={[styles.actionButton]}
           onPress={() => {
             const factor = 1.0;
-            const updated = { count: +(state.count + factor).toFixed(2) };
-            setState({ ...state, ...updated });
+            const updated = +(count + factor).toFixed(2);
+            setCount(updated);
           }}
         >
           <Text style={styles.actionButtonText}>+1</Text>
@@ -168,8 +168,8 @@ const RecordForm = ({onUpdate}) => {
           style={[styles.actionButton]}
           onPress={() => {
             const factor = 0.2;
-            const updated = { count: +(state.count + factor).toFixed(2) };
-            setState({ ...state, ...updated });
+            const updated = +(count + factor).toFixed(2);
+            setCount(updated);
           }}
         >
           <Text style={styles.actionButtonText}> +.2</Text>
@@ -194,39 +194,45 @@ const RecordForm = ({onUpdate}) => {
           style={styles.countInput}
           type="text"
           keyboardType="numeric"
-          value={state.count}
-          onChangeText={onTextChange("count")}
+          value={count}
+          onChangeText={setCount}
           placeholder="Count"
-        />
-      </View>
-
-      <View style={styles.actionsContainer}>
-        <Text style={styles.actionButtonText}>Date: </Text>
-
-        <TextInput
-          style={styles.dateInput}
-          type="text"
-          value={state.recordDate}
-          onChangeText={onTextChange("recordDate")}
-          placeholder="yyyy-mm-dd"
         />
       </View>
       <View>
         <Text style={styles.actionButtonText}>Comment</Text>
         <TextInput
+          style={styles.commentInput}
           type="text"
-          value={state.comment}
-          onChangeText={onTextChange("comment")}
+          value={comment}
+          onChangeText={setComment}
           placeholder="comment"
         />
       </View>
-
-      <Button
-        onPress={() => {
-          sendRecord();
-        }}
-        title="Submit"
-      />
+      <View>
+        <Button
+          onPress={() => {
+            sendRecord();
+          }}
+          title="Submit"
+        />
+      </View>
+      <View style={styles.actionsContainer}>
+        <Text>Date: </Text>
+        <DatePicker
+          onChange={setRecordDate}
+          value={recordDate}
+          onCalendarClose={calendarCheck}
+        />
+        <TouchableHighlight
+          style={[styles.actionButton]}
+          onPress={() => {
+            setRecordDate(sub(recordDate, { days: 1 }));
+          }}
+        >
+          <Text style={styles.actionButtonText}>-1</Text>
+        </TouchableHighlight>
+      </View>
     </View>
   );
 };
